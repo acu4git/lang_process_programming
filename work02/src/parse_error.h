7e030da482de2e6b47cf327e153f0cc8fc109795
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-01-13 00:56:59 +0900

    fff

--- work02/src/parse_error.h
+++ work02/src/parse_error.h
@@ -1,33 +1,34 @@
 #ifndef PARSE_ERROR_H
 #define PARSE_ERROR_H
 
-extern const char* ERRMSG_AND_NOT_FOUND;
-extern const char* ERRMSG_ARRAY_NOT_FOUND;
-extern const char* ERRMSG_BEGIN_NOT_FOUND;
-extern const char* ERRMSG_BOOLEAN_NOT_FOUND;
-extern const char* ERRMSG_BREAK_NOT_FOUND;
-extern const char* ERRMSG_CALL_NOT_FOUND;
-extern const char* ERRMSG_CHAR_NOT_FOUND;
-extern const char* ERRMSG_DIV_NOT_FOUND;
-extern const char* ERRMSG_DO_NOT_FOUND;
-extern const char* ERRMSG_ELSE_NOT_FOUND;
-extern const char* ERRMSG_END_NOT_FOUND;
-extern const char* ERRMSG_FALSE_NOT_FOUND;
-extern const char* ERRMSG_IF_NOT_FOUND;
-extern const char* ERRMSG_INTEGER_NOT_FOUND;
-extern const char* ERRMSG_NOT_NOT_FOUND;
-extern const char* ERRMSG_OF_NOT_FOUND;
-extern const char* ERRMSG_OR_NOT_FOUND;
-extern const char* ERRMSG_PROCEDURE_NOT_FOUND;
-extern const char* ERRMSG_PROGRAM_NOT_FOUND;
-extern const char* ERRMSG_READ_NOT_FOUND;
-extern const char* ERRMSG_READLN_NOT_FOUND;
-extern const char* ERRMSG_RETURN_NOT_FOUND;
-extern const char* ERRMSG_THEN_NOT_FOUND;
-extern const char* ERRMSG_TRUE_NOT_FOUND;
-extern const char* ERRMSG_VAR_NOT_FOUND;
-extern const char* ERRMSG_WHILE_NOT_FOUND;
-extern const char* ERRMSG_WRITE_NOT_FOUND;
-extern const char* ERRMSG_WRITELN_NOT_FOUND;
+// keyword error
+#define ERRMSG_AND_NOT_FOUND "Keyword 'and' is not found"
+#define ERRMSG_ARRAY_NOT_FOUND "Keyword 'array' is not found"
+#define ERRMSG_BEGIN_NOT_FOUND "Keyword 'begin' is not found"
+#define ERRMSG_BOOLEAN_NOT_FOUND "Keyword 'boolean' is not found"
+#define ERRMSG_BREAK_NOT_FOUND "Keyword 'break' is not found"
+#define ERRMSG_CALL_NOT_FOUND "Keyword 'call' is not found"
+#define ERRMSG_CHAR_NOT_FOUND "Keyword 'char' is not found"
+#define ERRMSG_DIV_NOT_FOUND "Keyword 'div' is not found"
+#define ERRMSG_DO_NOT_FOUND "Keyword 'do' is not found"
+#define ERRMSG_ELSE_NOT_FOUND "Keyword 'else' is not found"
+#define ERRMSG_END_NOT_FOUND "Keyword 'end' is not found"
+#define ERRMSG_FALSE_NOT_FOUND "Keyword 'false' is not found"
+#define ERRMSG_IF_NOT_FOUND "Keyword 'if' is not found"
+#define ERRMSG_INTEGER_NOT_FOUND "Keyword 'integer' is not found"
+#define ERRMSG_NOT_NOT_FOUND "Keyword 'not' is not found"
+#define ERRMSG_OF_NOT_FOUND "Keyword 'of' is not found"
+#define ERRMSG_OR_NOT_FOUND "Keyword 'or' is not found"
+#define ERRMSG_PROCEDURE_NOT_FOUND "Keyword 'procedure' is not found"
+#define ERRMSG_PROGRAM_NOT_FOUND "Keyword 'program' is not found"
+#define ERRMSG_READ_NOT_FOUND "Keyword 'read' is not found"
+#define ERRMSG_READLN_NOT_FOUND "Keyword 'readln' is not found"
+#define ERRMSG_RETURN_NOT_FOUND "Keyword 'return' is not found"
+#define ERRMSG_THEN_NOT_FOUND "Keyword 'then' is not found"
+#define ERRMSG_TRUE_NOT_FOUND "Keyword 'true' is not found"
+#define ERRMSG_VAR_NOT_FOUND "Keyword 'var' is not found"
+#define ERRMSG_WHILE_NOT_FOUND "Keyword 'while' is not found"
+#define ERRMSG_WRITE_NOT_FOUND "Keyword 'write' is not found"
+#define ERRMSG_WRITELN_NOT_FOUND "Keyword 'writeln' is not found"
 
 #endif
--- work02/src/parser.c
+++ work02/src/parser.c
@@ -1,9 +1,10 @@
 #include "parser.h"
 
+#include "parse_error.h"
 #include "scan.h"
 
 static int parse_program() {
-  if (token != TPROGRAM) return error("Keyword 'program' is not found");
+  if (token != TPROGRAM) return error(ERRMSG_PROGRAM_NOT_FOUND);
   token = scan();
   if (token != TNAME) return error("Program name is not found");
   token = scan();
@@ -17,9 +18,11 @@ static int parse_program() {
 static int parse_block() {}
 
 static int parse_variable_declaration() {
-  if (token != TVAR) return error("Keyword 'var' is not found");
+  if (token != TVAR) return error(ERRMSG_VAR_NOT_FOUND);
   token = scan();
   if (parse_variable_names() == ERROR) return ERROR;
+  if (token != TCOLON) return error("Colon is not found");
+  token = scan();
 }
 
 static int parse_variable_names() {
@@ -32,7 +35,22 @@ static int parse_variable_names() {
 }
 
 static int parse_variable_name() {
-  if (token != TNAME) return error("Variable name is not found");
+  if (token != TNAME) return error("Invalid variable name");
   token = scan();
   return NORMAL;
+}
+
+static int parse_type() {
+}
+
+static int parse_standard_type() {
+  if (token == TINTEGER || token == TBOOLEAN || token == TCHAR) {
+    token = scan();
+    return NORMAL;
+  }
+  return error("Standard type is not found");
+}
+
+static int parse_array_type() {
+  if (token != TARRAY) return error(ERRMSG_ARRAY_NOT_FOUND);
 }
