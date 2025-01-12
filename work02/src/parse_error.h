4e4f05fdeddf1c8902b4fc2aad07a72ab9bd31eb
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-01-12 23:34:28 +0900

    Keywordエラーの宣言

--- work02/src/parse_error.h
+++ work02/src/parse_error.h
@@ -1,4 +1,33 @@
 #ifndef PARSE_ERROR_H
 #define PARSE_ERROR_H
 
+extern const char* ERRMSG_AND_NOT_FOUND;
+extern const char* ERRMSG_ARRAY_NOT_FOUND;
+extern const char* ERRMSG_BEGIN_NOT_FOUND;
+extern const char* ERRMSG_BOOLEAN_NOT_FOUND;
+extern const char* ERRMSG_BREAK_NOT_FOUND;
+extern const char* ERRMSG_CALL_NOT_FOUND;
+extern const char* ERRMSG_CHAR_NOT_FOUND;
+extern const char* ERRMSG_DIV_NOT_FOUND;
+extern const char* ERRMSG_DO_NOT_FOUND;
+extern const char* ERRMSG_ELSE_NOT_FOUND;
+extern const char* ERRMSG_END_NOT_FOUND;
+extern const char* ERRMSG_FALSE_NOT_FOUND;
+extern const char* ERRMSG_IF_NOT_FOUND;
+extern const char* ERRMSG_INTEGER_NOT_FOUND;
+extern const char* ERRMSG_NOT_NOT_FOUND;
+extern const char* ERRMSG_OF_NOT_FOUND;
+extern const char* ERRMSG_OR_NOT_FOUND;
+extern const char* ERRMSG_PROCEDURE_NOT_FOUND;
+extern const char* ERRMSG_PROGRAM_NOT_FOUND;
+extern const char* ERRMSG_READ_NOT_FOUND;
+extern const char* ERRMSG_READLN_NOT_FOUND;
+extern const char* ERRMSG_RETURN_NOT_FOUND;
+extern const char* ERRMSG_THEN_NOT_FOUND;
+extern const char* ERRMSG_TRUE_NOT_FOUND;
+extern const char* ERRMSG_VAR_NOT_FOUND;
+extern const char* ERRMSG_WHILE_NOT_FOUND;
+extern const char* ERRMSG_WRITE_NOT_FOUND;
+extern const char* ERRMSG_WRITELN_NOT_FOUND;
+
 #endif
