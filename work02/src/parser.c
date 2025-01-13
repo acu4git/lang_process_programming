6e57b50f269987b674b8ae1847fedc3eeae73b0c
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-01-13 16:59:28 +0900

    aaaa

--- work02/src/parser.c
+++ work02/src/parser.c
@@ -3,47 +3,69 @@
 #include "parse_error.h"
 #include "scan.h"
 
-static int parse_program() {
+extern int token;
+
+int parse_program() {
   if (token != TPROGRAM) return error(ERRMSG_PROGRAM_NOT_FOUND);
   token = scan();
   if (token != TNAME) return error("Program name is not found");
   token = scan();
-  if (token != TSEMI) return error("Semicolon is not found");
+  if (token != TSEMI) return error("Semicolon(;) is not found");
   if (parse_block() == ERROR) return ERROR;
   if (token != TDOT) return error("Period is not found at the end of program");
   token = scan();
   return NORMAL;
 }
 
-static int parse_block() {}
-
-static int parse_variable_declaration() {
-  if (token != TVAR) return error(ERRMSG_VAR_NOT_FOUND);
-  token = scan();
-  if (parse_variable_names() == ERROR) return ERROR;
-  if (token != TCOLON) return error("Colon is not found");
-  token = scan();
+int parse_block() {
+  while (token == TVAR || token == TPROCEDURE) {
+    if (token == TVAR && parse_variable_declaration() == ERROR)
+      return ERROR;
+    else if (token == TPROCEDURE && parse_subprogram_declaration() == ERROR)
+      return ERROR;
+  }
+  if (parse_compound_statement() == ERROR) return ERROR;
+  return NORMAL;
 }
 
-static int parse_variable_names() {
-  if (parse_variable_name() == ERROR) return ERROR;
-  while (token == TCOMMA) {
+int parse_variable_declaration() {
+  if (token != TVAR) return error(ERRMSG_VAR_NOT_FOUND);
+  token = scan();
+  do {
+    if (parse_variable_names() == ERROR) return ERROR;
+    if (token != TCOLON) return error("Colon(':') is not found");
     token = scan();
+    if (parse_type() == ERROR) return ERROR;
+    if (token != TSEMI) return error("Semicolon(;) is not found");
+    token = scan();
+  } while (token == TNAME);
+  return NORMAL;
+}
+
+int parse_variable_names() {
+  while (1) {
     if (parse_variable_name() == ERROR) return ERROR;
+    if (token != TCOMMA) break;
+    token = scan();
   }
   return NORMAL;
 }
 
-static int parse_variable_name() {
-  if (token != TNAME) return error("Invalid variable name");
+int parse_variable_name() {
+  if (token != TNAME) return error("Variable name is invalid");
   token = scan();
   return NORMAL;
 }
 
-static int parse_type() {
+int parse_type() {
+  if ((token == TINTEGER || token == TBOOLEAN || token == TCHAR) && parse_standard_type() == ERROR)
+    return ERROR;
+  else if (token == TARRAY && parse_array_type() == ERROR)
+    return ERROR;
+  return NORMAL;
 }
 
-static int parse_standard_type() {
+int parse_standard_type() {
   if (token == TINTEGER || token == TBOOLEAN || token == TCHAR) {
     token = scan();
     return NORMAL;
@@ -51,6 +73,107 @@ static int parse_standard_type() {
   return error("Standard type is not found");
 }
 
-static int parse_array_type() {
+int parse_array_type() {
   if (token != TARRAY) return error(ERRMSG_ARRAY_NOT_FOUND);
+  token = scan();
+  if (token != TLSQPAREN) return error("Left square bracket('[') is not found");
+  token = scan();
+  if (token != TINTEGER) return error("Integer is not found");
+  token = scan();
+  if (token != TRSQPAREN) return error("Right square bracket(']') is not found");
+  token = scan();
+  if (token != TOF) return error(ERRMSG_OF_NOT_FOUND);
+  token = scan();
+  if (parse_standard_type() == ERROR) return ERROR;
+  token = scan();
+  return NORMAL;
+}
+
+int parse_subprogram_declaration() {
+  if (token != TPROCEDURE) return error(ERRMSG_PROCEDURE_NOT_FOUND);
+  token = scan();
+  if (parse_procedure_name() == ERROR) return ERROR;
+  if (token == TLPAREN && parse_formal_parameters() == ERROR) return ERROR;
+  if (token != TSEMI) return error("Semicolon(';') is not found");
+  token = scan();
+  if (token == TVAR && parse_variable_declaration() == ERROR) return ERROR;
+  if (parse_compound_statement() == ERROR) return ERROR;
+  if (token != TSEMI) return error("Semicolon(';') is not found");
+  return NORMAL;
+}
+
+int parse_procedure_name() {
+  if (token != TNAME) return error("Procedure name is not found");
+  token = scan();
+  return NORMAL;
+}
+
+int parse_formal_parameters() {
+  if (token != TLPAREN) return error("Left parenthesis('(') is not found");
+  token = scan();
+  while (token == TNAME) {
+    if (parse_variable_names() == ERROR) return ERROR;
+    if (token != TCOLON) return error("Colon(':') is not found");
+    token = scan();
+    if (parse_type() == ERROR) return ERROR;
+    if (token != TSEMI) break;
+    token = scan();
+  }
+  if (token != TRPAREN) return error("Right parenthesis(')') is not found");
+  token = scan();
+  return NORMAL;
+}
+
+int parse_compound_statement() {
+  if (token != TBEGIN) return error(ERRMSG_BEGIN_NOT_FOUND);
+  token = scan();
+  while (1) {
+    if (parse_statement() == ERROR) return ERROR;
+    if (token != TSEMI) break;
+    token = scan();
+  }
+  if (token != TEND) return error(ERRMSG_END_NOT_FOUND);
+  token = scan();
+  return NORMAL;
+}
+
+int parse_statement() {
+  if (token == TNAME && parse_assignment_statement() == ERROR)
+    return ERROR;
+  else if (token == TIF && parse_condition_statement() == ERROR)
+    return ERROR;
+  else if (token == TWHILE && parse_iteration_statement() == ERROR)
+    return ERROR;
+  else if (token == TBREAK && parse_exit_statement() == ERROR)
+    return ERROR;
+  else if (token == TCALL && parse_call_statement() == ERROR)
+    return ERROR;
+  else if (token == TRETURN && parse_return_statement() == ERROR)
+    return ERROR;
+  else if ((token == TREAD || token == TREADLN) && parse_input_statement() == ERROR)
+    return ERROR;
+  else if ((token == TWRITE || token == TWRITELN) && parse_output_statement() == ERROR)
+    return ERROR;
+  else if (token == TBEGIN && parse_compound_statement() == ERROR)
+    return ERROR;
+  return NORMAL;
+}
+
+int parse_condition_statement() {
+  if (token != TIF) return error(ERRMSG_IF_NOT_FOUND);
+  token = scan();
+  if (parse_expression() == ERROR) return ERROR;
+  if (token != TTHEN) return error(ERRMSG_THEN_NOT_FOUND);
+  token = scan();
+  if (parse_statement() == ERROR) return ERROR;
+  while (token == TELSE) {
+    if (parse_statement() == ERROR) return ERROR;
+  }
+  return NORMAL;
+}
+
+int parse_iteration_statement() {
+  if (token != TWHILE) return error(ERRMSG_WHILE_NOT_FOUND);
+  if (parse_expression() == ERROR) return ERROR;
+  if (token != TDO) return error(ERRMSG_DO_NOT_FOUND);
 }
--- work02/src/parser.h
+++ work02/src/parser.h
@@ -4,40 +4,38 @@
 #define NORMAL 0
 #define ERROR -1
 
-extern int token;
-
-static int parse_program();                  // プログラム
-static int parse_block();                    // ブロック
-static int parse_variable_declaration();     // 変数宣言部
-static int parse_variable_names();           // 変数名の並び
-static int parse_variable_name();            // 変数名
-static int parse_type();                     // 型
-static int parse_standard_type();            // 標準型
-static int parse_array_type();               // 配列型
-static int parse_subprogram_declaration();   // 副プログラム宣言
-static int parse_procedure_name();           // 手続き名
-static int parse_formal_parameters();        // 仮引数名
-static int parse_compound_statement();       // 複合文
-static int parse_statement();                // 文
-static int parse_condition_statement();      // 分岐文
-static int parse_iteration_statement();      // 繰り返し文
-static int parse_exit_statement();           // 脱出文
-static int parse_call_statement();           // 手続き呼び出し文
-static int parse_expressions();              // 式の並び
-static int parse_return_statement();         // 戻り文
-static int parse_assignment_statement();     // 代入文
-static int parse_left_part();                // 左辺部
-static int parse_variable();                 // 変数
-static int parse_expression();               // 式
-static int parse_simple_expression();        // 単純式
-static int parse_term();                     // 項
-static int parse_factor();                   // 因子
-static int parse_constant();                 // 定数
-static int parse_multiplicative_operator();  // 乗法演算子
-static int parse_madditive_operator();       // 加法演算子
-static int parse_relational_operator();      // 関係演算子
-static int parse_input_statement();          // 入力文
-static int parse_output_statement();         // 出力文
-static int parse_empty_statement();          // 空文
+int parse_program();                  // プログラム
+int parse_block();                    // ブロック
+int parse_variable_declaration();     // 変数宣言部
+int parse_variable_names();           // 変数名の並び
+int parse_variable_name();            // 変数名
+int parse_type();                     // 型
+int parse_standard_type();            // 標準型
+int parse_array_type();               // 配列型
+int parse_subprogram_declaration();   // 副プログラム宣言
+int parse_procedure_name();           // 手続き名
+int parse_formal_parameters();        // 仮引数名
+int parse_compound_statement();       // 複合文
+int parse_statement();                // 文
+int parse_condition_statement();      // 分岐文
+int parse_iteration_statement();      // 繰り返し文
+int parse_exit_statement();           // 脱出文
+int parse_call_statement();           // 手続き呼び出し文
+int parse_expressions();              // 式の並び
+int parse_return_statement();         // 戻り文
+int parse_assignment_statement();     // 代入文
+int parse_left_part();                // 左辺部
+int parse_variable();                 // 変数
+int parse_expression();               // 式
+int parse_simple_expression();        // 単純式
+int parse_term();                     // 項
+int parse_factor();                   // 因子
+int parse_constant();                 // 定数
+int parse_multiplicative_operator();  // 乗法演算子
+int parse_madditive_operator();       // 加法演算子
+int parse_relational_operator();      // 関係演算子
+int parse_input_statement();          // 入力文
+int parse_output_statement();         // 出力文
+int parse_empty_statement();          // 空文
 
 #endif
